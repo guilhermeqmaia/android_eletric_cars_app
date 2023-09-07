9e274facabc5dac314a2a24afd3e6d248bb842b7
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2023-09-07 14:06:14 -0300

    feat: add empty state

--- app/src/main/java/com/example/eletric_cars_app/presenter/CarFragment.kt
+++ app/src/main/java/com/example/eletric_cars_app/presenter/CarFragment.kt
@@ -1,15 +1,21 @@
 package com.example.eletric_cars_app.presenter
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.opengl.Visibility
 import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eletric_cars_app.R
@@ -31,8 +37,11 @@ class CarFragment : Fragment() {
     lateinit var fabCalculate : FloatingActionButton
     lateinit var carsList : RecyclerView
     lateinit var progressBar : ProgressBar
+    lateinit var emptyStateImage : ImageView
+    lateinit var emptyStateText : TextView
+    lateinit var emptyStateRetryButton: Button
     var carsArray :  ArrayList<Car> = ArrayList()
-    val carFactory = CarFactory()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,18 +56,49 @@ class CarFragment : Fragment() {
         setupView(view)
         setupListViewData()
         onTapCalculateButton()
-        execute()
+        execute(view.context)
     }
 
-    private fun execute() {
-        GetCarInformation().execute("https://igorbag.github.io/cars-api/cars.json")
+    private fun execute(context: Context) {
+        val isConnected = isInternetOn(context)
+        if(isConnected) {
+            GetCarInformation().execute("https://igorbag.github.io/cars-api/cars.json")
+            onExecute()
+        } else onEmptyState()
+    }
+
+    private fun onExecute() {
         progressBar.visibility = View.VISIBLE
+        emptyStateImage.visibility = View.GONE
+        emptyStateText.visibility = View.GONE
+        emptyStateRetryButton.visibility = View.GONE
+    }
+
+    private fun onEmptyState() {
+        progressBar.visibility = View.GONE
+        carsList.visibility = View.GONE
+        fabCalculate.visibility = View.GONE
+        emptyStateImage.visibility = View.VISIBLE
+        emptyStateText.visibility = View.VISIBLE
+        emptyStateRetryButton.visibility = View.VISIBLE
+    }
+
+    private fun onSuccessState() {
+        carsList.visibility = View.VISIBLE
+        fabCalculate.visibility = View.VISIBLE
+        progressBar.visibility = View.GONE
+        emptyStateImage.visibility = View.GONE
+        emptyStateText.visibility = View.GONE
+        emptyStateRetryButton.visibility = View.GONE
     }
 
     private fun setupView(view: View) {
         fabCalculate = view.findViewById(R.id.fab_calculate)
         carsList = view.findViewById(R.id.rv_cars_list)
         progressBar = view.findViewById(R.id.pb_loader)
+        emptyStateImage = view.findViewById(R.id.iv_empty_state)
+        emptyStateText = view.findViewById(R.id.tv_connection_off)
+        emptyStateRetryButton = view.findViewById(R.id.btn_retry)
     }
 
     private fun setupListViewData() {
@@ -69,6 +109,28 @@ class CarFragment : Fragment() {
     private fun onTapCalculateButton() {
         fabCalculate.setOnClickListener {
            startActivity(Intent(context, CalculateAutonomyActivity::class.java))
+        }
+        emptyStateRetryButton.setOnClickListener {
+            context?.let { it1 -> execute(it1) }
+        }
+    }
+
+    private fun isInternetOn(context: Context?) : Boolean {
+        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            val networkInfo = connectivityManager.activeNetworkInfo?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected()
         }
     }
 
@@ -130,13 +192,8 @@ class CarFragment : Fragment() {
                 Log.e("Error", ex.message.toString())
             } finally {
                 setupListViewData()
-                onEndRequest()
+                onSuccessState()
             }
         }
-    }
-
-    private fun onEndRequest() {
-        progressBar.visibility = View.GONE
-        carsList.visibility = View.VISIBLE
     }
 }
